7ffe9213472d48f535fd39802ae91c7e8f770dd4
Nayanish D Hinge <nayanish.hinge@gmail.com>
2014-02-15 15:42:58 -0800

    Initial commit for maximum sum subarray
    initial commit

---
+++ DynamicProgramming/src/com/hinge/arrayproblems/MaximumSumSubSequence.java
@@ -0,0 +1,12 @@
+package com.hinge.arrayproblems;
+
+/**
+ * Given an integer array, find the sub array which has maximum sum. The array
+ * would have both negative and positive numbers.
+ * 
+ * @author naynish
+ * 
+ */
+public class MaximumSumSubSequence {
+
+}
